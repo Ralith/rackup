f478309f77e6ace7c797ee8eeff885e6c470f326
Benjamin Saunders <ben.e.saunders@gmail.com>
2017-12-31 16:46:18 -0800

    Walk and dump to rdedup

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 with import <nixpkgs> { };
 stdenv.mkDerivation {
   name = "sg-car";
-  buildInputs = with pkgs; [ rust-nightly gpgme ];
+  buildInputs = with pkgs; [ rust-nightly pkgconfig libsodium zstd xz ];
   shellHook = ''
     export CARGO_INCREMENTAL=1
     export GPGME_CONFIG="${gpgme.dev}/bin/gpgme-config"
--- src/main.rs
+++ src/main.rs
@@ -1,134 +1,295 @@
-#![recursion_limit = "1024"]   // for error-chain
+#![feature(integer_atomics)]
 
 extern crate clap;
-extern crate indicatif;
-extern crate gpgme;
-#[macro_use]
-extern crate error_chain;
-extern crate futures;
-extern crate futures_cpupool;
-extern crate zstd;
+extern crate yapb;
+extern crate termion;
+extern crate failure;
+extern crate rayon;
+extern crate rdedup_lib as rdedup;
+extern crate url;
+extern crate rpassword;
 
-use std::{fs, fmt};
+use std::{fmt, thread, cmp};
+use std::fs::{self, File};
+use std::io::{self, Read, Write};
 use std::path::PathBuf;
-use std::sync::Arc;
+use std::sync::{Arc, mpsc};
+use std::sync::atomic::{AtomicU64, Ordering};
+use std::time::{Duration, Instant};
 
-use clap::{Arg, ArgGroup, App};
-use indicatif::{ProgressBar, MultiProgress, ProgressStyle};
-use error_chain::ChainedError;
-use futures::{Future, Stream, stream};
-use futures_cpupool::CpuPool;
+use clap::{Arg, App};
+use yapb::{Spinner, Progress};
+use failure::{Fail, Error, ResultExt};
+use rayon::prelude::*;
+use url::Url;
 
-error_chain! {}
+type Result<T> = std::result::Result<T, Error>;
 
-pub struct PrettyErr<'a, E: ChainedError>(&'a E);
-
-impl<'a, E: ChainedError> fmt::Display for PrettyErr<'a, E> {
+pub struct PrettyErr<'a>(&'a Fail);
+impl<'a> fmt::Display for PrettyErr<'a> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         fmt::Display::fmt(&self.0, f)?;
-        for x in self.0.iter().skip(1) {
-            f.pad(": ")?;
-            fmt::Display::fmt(&x, f)?;
+        let mut x: &Fail = self.0;
+        while let Some(cause) = x.cause() {
+            f.write_str(": ")?;
+            fmt::Display::fmt(&cause, f)?;
+            x = cause;
         }
         Ok(())
     }
 }
 
-/// Compact display wrapper for the full cause chain of an error.
-pub fn pretty_err<E: ChainedError>(x: &E) -> PrettyErr<E> { PrettyErr(x) }
+pub trait ErrorExt {
+    fn pretty(&self) -> PrettyErr;
+}
 
+impl ErrorExt for Error {
+    fn pretty(&self) -> PrettyErr { PrettyErr(self.cause()) }
+}
 
 fn main() {
     let args = App::new("rackup")
         .version("0.1")
         .author("Benjamin Saunders <ben.e.saunders@gmail.com>")
         .about("A back-up tool")
-        .arg(Arg::with_name("ID")
-             .help("Identifier for this backup")
-             .required(true))
+        .arg(Arg::with_name("REPO")
+             .help("rdedup repository URL to write to")
+             .required(true)
+             .validator(|x| Url::parse(&x).map(|_| ()).map_err(|x| x.to_string())))
         .arg(Arg::with_name("SOURCE")
-             .help("Location of data to back up")
+             .help("location of data to back up")
              .default_value("."))
-        .arg(Arg::with_name("local dest")
-             .takes_value(true)
-             .help("Destination for backed-up data on the local filesystem")
-             .short("l")
-             .long("local"))
-        .arg(Arg::with_name("key")
-             .takes_value(true)
-             .help("GPG public key to encrypt to")
-             .short("k")
-             .long("key"))
-        .group(ArgGroup::with_name("destination")
-               .args(&["local dest"])
-               .required(true))
+        .arg(Arg::with_name("NAME")
+             .long("name")
+             .short("n")
+             .help("rdedup name to write to")
+             .default_value("backup"))
         .get_matches();
 
     if let Err(e) = run(args) {
-        eprintln!("{}", pretty_err(&e));
+        eprintln!("FATAL: {}", e.pretty());
         ::std::process::exit(1);
     }
 }
 
 fn run<'a>(args: clap::ArgMatches<'a>) -> Result<()> {
-    let ctx = Arc::new(Context::new(args)?);
-    
-    let walk = walk_dir(ctx.clone(), ctx.prefix.clone());
+    let stdout = io::stdout();
+    let mut stdout = stdout.lock();
 
-    ctx.progress.join_and_clear().unwrap();
-    println!("progress finished");
-    walk.wait().unwrap();
+    let repo = rdedup::Repo::open(&Url::parse(args.value_of("REPO").unwrap()).unwrap(), None)
+        .context("failed to open rdedup repo")?;
+    let encrypt = repo.unlock_encrypt(&|| {
+        println!("Enter passprase: ");
+        rpassword::read_password()
+    }).context("failed to prepare encryption for rdedup repo")?;
+
+    let index = {
+        let source = PathBuf::from(args.value_of_os("SOURCE").unwrap());
+        let (send, recv) = mpsc::sync_channel(16);
+        let source2 = source.clone();
+        let thread = thread::spawn(move || {
+            let index = walk_dir(send, source2);
+            index
+        });
+        let index;
+        let mut spinner = yapb::Snake::new();
+        loop {
+            match recv.recv_timeout(Duration::from_millis(100)) {
+                Err(mpsc::RecvTimeoutError::Timeout) => {
+                    write!(stdout, "{}{}{} indexing {}",
+                           termion::cursor::Left(!0), termion::clear::CurrentLine,
+                           spinner, source.display()).unwrap();
+                    stdout.flush().unwrap();
+                    spinner.step(1);
+                }
+                Err(mpsc::RecvTimeoutError::Disconnected) => {
+                    index = thread.join().unwrap()
+                        .with_context(|_| format!("failed to index {}", source.display()))?;
+                    break;
+                }
+                Ok(e) => {
+                    eprintln!("ERROR: {}", e.pretty());
+                }
+            }
+        }
+        index
+    };
+
+    writeln!(stdout, "{}indexed {} files, {}B",
+             termion::cursor::Left(!0),
+             index.files.len(),
+             yapb::prefix::Binary(index.bytes as f64)).unwrap();
+
+    let total_bytes = index.bytes;
+    let start_time = Instant::now();
+    let (reader, status) = DataReader::new(index);
+    let name = args.value_of("NAME").unwrap().to_owned();
+    let thread = thread::spawn(move || -> Result<(u64, usize)> {
+        let stats = repo.write(&name, reader, &encrypt)?;
+        Ok((stats.new_bytes, stats.new_chunks))
+    });
+
+    let mut bar = yapb::Bar::new();
+    let mut spinner = yapb::Snake::new();
+    let mut throughput = None;
+    let mut last_bytes = 0;
+    loop {
+        match status.errors.recv_timeout(Duration::from_millis(100)) {
+            Err(mpsc::RecvTimeoutError::Timeout) => {
+                let (width, _) = termion::terminal_size().unwrap();
+                let bytes = status.bytes.load(Ordering::Relaxed);
+                let rate = (bytes - last_bytes) as f32;
+                if throughput.is_none() {
+                    throughput = Some(yapb::MovingAverage::new(0.05, rate));
+                } else {
+                    throughput.as_mut().unwrap().update(rate);
+                }
+                last_bytes = bytes;
+                bar.set(cmp::min(total_bytes, bytes) as f32 / total_bytes as f32);
+                write!(stdout, "{}{} {:3}% [{:width$}] {}B/s{}",
+                       termion::cursor::Left(!0), spinner, (bar.get() * 100.0) as u32, bar,
+                       yapb::prefix::Binary(throughput.as_ref().unwrap().get() as f64),
+                       termion::clear::UntilNewline,
+                       width = width as usize - 20).unwrap();
+                stdout.flush().unwrap();
+                spinner.step(1);
+            }
+            Err(mpsc::RecvTimeoutError::Disconnected) => {
+                let (bytes, chunks) = thread.join().unwrap().context("write failed")?;
+                let time = Instant::now() - start_time;
+                writeln!(stdout, "{}{}backup completed in {}s: wrote {}B in {} chunks",
+                         termion::cursor::Left(!0), termion::clear::AfterCursor,
+                         yapb::prefix::Scientific(time.as_secs() as f64 + time.subsec_nanos() as f64 * 1e-9),
+                         yapb::prefix::Binary(bytes as f64), chunks).unwrap();
+                break;
+            }
+            Ok(e) => {
+                eprintln!("ERROR: {}{}", e.pretty(), termion::clear::UntilNewline);
+            }
+        }
+    }
+
     Ok(())
 }
 
-fn walk_dir(ctx: Arc<Context>, dir: PathBuf) -> Box<Future<Item=(), Error=()> + Send> { // TODO: No errors!
-    let pb = ctx.progress.add(ProgressBar::new_spinner());
-    pb.set_style(ProgressStyle::default_spinner()
-                 .template("{spinner} {msg}"));
-    let ctx2 = ctx.clone();
-    Box::new(ctx2.pool.spawn_fn(move || {
-        let mut spawned = Vec::new();
-        if let Err(e) = (|| -> Result<()> {
-            for entry in fs::read_dir(&dir).chain_err(|| format!("failed to access {}", dir.display()))? {
-                let result: Result<()> = (|| {
-                    let entry = entry.chain_err(|| format!("failed to access entry of {}", dir.display()))?;
-                    pb.set_message(&format!("checking {}", entry.path().strip_prefix(&ctx.prefix).unwrap().display()));
-                    if entry.file_type().chain_err(|| "failed to determine directory entry type")?.is_dir() {
-                        spawned.push(walk_dir(ctx.clone(), entry.path()));
-                    } else {
-                        let meta = entry.metadata().chain_err(|| format!("failed to access metadata for {}", entry.path().display()))?;
-                        //spawned.push(check(&pool2, m.clone(), entry.path(), meta));
-                    }
-                    Ok(())
-                })();
-                if let Err(e) = result {
-                    eprintln!("ERROR: {}", pretty_err(&e));
-                }
+struct Index {
+    files: Vec<PathBuf>,
+    bytes: u64,
+}
+
+impl Index {
+    fn new() -> Self { Self {
+        files: Vec::new(),
+        bytes: 0,
+    }}
+
+    fn push(&mut self, path: PathBuf, meta: &fs::Metadata) {
+        self.files.push(path);
+        self.bytes += meta.len();
+    }
+}
+
+impl ::std::ops::Add for Index {
+    type Output = Index;
+
+    fn add(mut self, other: Index) -> Index {
+        self.files.extend(other.files.into_iter());
+        self.bytes += other.bytes;
+        self
+    }
+}
+
+impl ::std::iter::Sum for Index {
+    fn sum<I>(iter: I) -> Self
+        where I: Iterator<Item = Index>
+    {
+        iter.fold(Index::new(), |x, y| x + y)
+    }
+}
+
+fn walk_dir(errs: mpsc::SyncSender<Error>, dir: PathBuf) -> Result<Index> {
+    let mut index = Index::new();
+    let subdirs = fs::read_dir(&dir).with_context(|_| format!("failed to access {}", dir.display()))
+        ?.map(|entry| {
+            let entry = entry.with_context(|_| format!("failed to access entry of {}", dir.display()))?;
+            let path = entry.path();
+            if entry.file_type().context("failed to determine directory entry type")?.is_dir() {
+                Ok(Some(path))
+            } else {
+                let meta = entry.metadata()
+                    .with_context(|_| format!("failed to access metadata for {}", path.display()))?;
+                index.push(path, &meta);
+                Ok(None)
             }
-            Ok(())
-        })() {
-            eprintln!("ERROR: {}", pretty_err(&e));
-        }
-        pb.finish_and_clear();
-        stream::futures_unordered(spawned)
-            .for_each(|()| Ok(()))
-    }))
-}
-
-struct Context {
-    gpg: gpgme::Token,
-    pool: CpuPool,
-    progress: Arc<MultiProgress>,
-    prefix: PathBuf,
-}
-
-impl Context {
-    pub fn new<'a>(args: clap::ArgMatches<'a>) -> Result<Context> {
-        Ok(Context {
-            gpg: gpgme::init(),
-            pool: CpuPool::new_num_cpus(),
-            progress: Arc::new(MultiProgress::new()),
-            prefix: args.value_of_os("SOURCE").unwrap().into(),
         })
+        .filter_map(|x| x.map(|x| x.map(Ok)).unwrap_or_else(|x| Some(Err(x))) )
+        .collect::<Vec<_>>()
+        .into_par_iter()
+        .filter_map(|dir| match dir.and_then(|x| walk_dir(errs.clone(), x)) {
+            Err(e) => { errs.send(e).unwrap(); None }
+            Ok(x) => Some(x)
+        })
+        .sum::<Index>();
+    Ok(subdirs + index)
+}
+
+struct DataReader {
+    index: Index,
+    offsets: Vec<u64>,
+    next: usize,
+    file: Option<File>,
+    bytes: Arc<AtomicU64>,
+    errors: mpsc::SyncSender<Error>
+}
+
+struct ReadStatus {
+    bytes: Arc<AtomicU64>,
+    errors: mpsc::Receiver<Error>,
+}
+
+impl DataReader {
+    fn new(index: Index) -> (Self, ReadStatus) {
+        let bytes = Arc::new(AtomicU64::new(0));
+        let (err_send, err_recv) = mpsc::sync_channel(16);
+        let result = Self {
+            offsets: Vec::with_capacity(index.files.len()),
+            index,
+            next: 0,
+            file: None,
+            bytes: bytes.clone(),
+            errors: err_send,
+        };
+        let status = ReadStatus {
+            bytes,
+            errors: err_recv,
+        };
+        (result, status)
+    }
+}
+
+impl Read for DataReader {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        loop {
+            while self.file.is_none() {
+                if self.next == self.index.files.len() { return Ok(0); }
+                match File::open(&self.index.files[self.next])
+                    .with_context(|_| format!("couldn't open {}", self.index.files[self.next].display()))
+                {
+                    Ok(file) => { self.file = Some(file); }
+                    Err(e) => { self.errors.send(e.into()).unwrap(); }
+                }
+                self.offsets.push(self.bytes.load(Ordering::Relaxed));
+                self.next += 1;
+            }
+            // TODO: Write index
+            // TODO: Handle symlinks
+            match self.file.as_mut().unwrap().read(buf)
+                .with_context(|_| format!("failed reading {}", self.index.files[self.next-1].display()))
+            {
+                Ok(0) => { self.file = None; }
+                Ok(n) => { self.bytes.fetch_add(n as u64, Ordering::Relaxed); return Ok(n); }
+                Err(e) => { self.errors.send(e.into()).unwrap(); }
+            }
+        }
     }
 }
